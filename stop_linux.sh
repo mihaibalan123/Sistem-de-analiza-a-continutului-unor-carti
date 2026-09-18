8c00f215935b8aa009735a724647470e29eccec7
Mihai Balan <mihaibalan568@gmail.com>
2026-09-18 15:43:21 +0300

    adaugare scripturi de oprire a aplicatiei

---
+++ stop_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "======================================================="
+echo "    Oprim Sistemul de Analiza a Continutului..."
+echo "======================================================="
+echo ""
+
+docker-compose stop
+
+echo ""
+echo "Aplicatia a fost oprita cu succes!"
+echo "Toate cartile analizate raman in siguranta."
+echo "======================================================="
+echo ""
---
+++ stop_windows.bat
@@ -0,0 +1,14 @@
+@echo off
+echo =======================================================
+echo     Oprim Sistemul de Analiza a Continutului...
+echo =======================================================
+echo.
+
+docker-compose stop
+
+echo.
+echo Aplicatia a fost oprita cu succes!
+echo Toate cartile analizate raman in siguranta.
+echo =======================================================
+echo.
+pause
